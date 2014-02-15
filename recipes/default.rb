6bc6d1118f008d103d61e75a59aa77c4a7d7632a
Jean Mertz <jean@mertz.fm>
2014-02-15 21:52:21 +0100

    Remove unneeded package requirements

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,14 +19,6 @@
 
 include_recipe 'build-essential'
 
-value_for_platform_family(
-  'debian' => ['libxml2-dev', 'libxslt1-dev'],
-  'rhel' => ['libxml2-devel', 'libxslt-devel'],
-).each do |pkg|
-  r = package( pkg ) { action :nothing }
-  r.run_action( :install )
-end
-
 chef_gem 'fog' do
   version node['dnsimple']['fog_version']
   action :install
